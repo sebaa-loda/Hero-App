4b8f2807c202de8898b4ac947d49313e286af0b2
Sebaa <sebaamdq96@gmail.com>
2024-03-14 16:54:12 -0300

    add context

---
+++ src/auth/context/authReducer.js
@@ -0,0 +1,25 @@
+import {types} from '../types/types'
+
+export const authReducer = ( state = {}, action) => {
+
+
+
+  switch (action.type) {
+    case types.login:
+      return {
+        ...state,
+        logged: true,
+        name: action.payload
+      }
+      
+    case types.logout:
+      return {
+        ...state,
+        logged: false,
+        name: action.payload
+      }
+  
+    default:
+      return state
+  }
+}
--- src/heroes/pages/Search.jsx
+++ src/heroes/pages/Search.jsx
@@ -2,15 +2,21 @@ import { HeroCard } from "../components";
 import { useForm } from "../../hooks/useform";
 import { useLocation, useNavigate } from "react-router-dom";
 import queryString from "query-string";
+import { getHeroByName } from "../helpers";
 
 export const Search = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
   const { q = '' } = queryString.parse(location.search);
+  const heroes = getHeroByName(q)
+
+  const showSearch = (q.lenght === 0);
+  const showError = (q.lenght > 0) && heroes.length === 0
+
 
   const { searchText, onInputChange } = useForm({
-    searchText: "",
+    searchText: q
   });
 
   const onSearchSubmit = (event) => {
@@ -48,12 +54,23 @@ export const Search = () => {
           <h4>Results</h4>
           <hr />
 
-          <div className="alert alert-primary">Search a hero</div>
-          <div className="alert alert-danger">
+          <div 
+          className="alert alert-primary"
+          style={{display: showSearch ? '' : 'none'}}
+          >Search a hero</div>
+          <div 
+          className="alert alert-danger"
+          style={{display: showError ? '' : 'none'}}
+          >
             No hero with <b>{q}</b>
           </div>
 
-          <HeroCard />
+          {
+            heroes.map(hero => (
+                <HeroCard key={hero.id} {...hero}/>
+            ))
+          }
+        
         </div>
       </div>
     </div>
